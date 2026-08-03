509d86c364dbc678fd0d8b595d410b5dd45d6569
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-08-03 17:35:10 +0000

    feat: define post schema with fields for title, description, inExchangeFor, rating, images, and author

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,5 +1,32 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
 const postschema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  inExchangeFor: {
+    type: String,
+    trim: true
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0
+  },
+  images: [String],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
 
-})
+const postModel = mongoose.model("Post", postschema);
+
+export default postModel;
