edae3b3f4af7a2844ad905ad680cb8aed7a601ab
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-09-22 15:42:21 +0000

    feat: improve error handling and validation in post creation and update

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,31 +1,32 @@
 import mongoose from "mongoose";
-import Post from "../models/postModel.js"
-import { uploadImages, deleteImages } from "../services/imageService.js"
-import { validateCoordinates } from "../utils/validateCoordinates.js"
+import Post from "../models/postModel.js";
+import { uploadImages, deleteImages } from "../services/imageService.js";
+import { validateCoordinates } from "../utils/validateCoordinates.js";
 
 export const createPost = async (req, res) => {
-  try{
-    const { title, type, description, inExchangeFor, longitude, latitude} = req.body;
-    const {_id} = req.user;
+  try {
+    const { title, type, description, inExchangeFor, longitude, latitude } =
+      req.body;
+    const { _id } = req.user;
 
-    if(!req?.files?.length){
+    if (!req?.files?.length) {
       return res.status(400).json({
         success: false,
-        message: "Images required."
-      })
+        message: "Images required.",
+      });
     }
 
-    if(!title || !type){
+    if (!title || !type) {
       return res.status(400).json({
         success: false,
-        message: "Provide both Title and Type."
+        message: "Provide both Title and Type.",
       });
-    };
+    }
 
-    if(!["online", "offline"].includes(type)){
+    if (!["online", "offline"].includes(type)) {
       return res.status(400).json({
         success: false,
-        message: "Type is invalid."
+        message: "Type is invalid.",
       });
     }
 
@@ -36,20 +37,18 @@ export const createPost = async (req, res) => {
     const lon = Number(longitude);
     const lat = Number(latitude);
 
-
-
-    if(type === "offline" && !hasLocation){
+    if (type === "offline" && !hasLocation) {
       return res.status(400).json({
         success: false,
-        message: "Provide location for offline barters."
-      })
+        message: "Provide location for offline barters.",
+      });
     }
 
-    if(hasLocation && !validateCoordinates(lat, lon)) {
+    if (hasLocation && !validateCoordinates(lat, lon)) {
       return res.status(400).json({
         success: false,
-        message: "Invalid location coordinates."
-      })
+        message: "Invalid location coordinates.",
+      });
     }
 
     const uploadedUrls = await uploadImages(req.files);
@@ -60,103 +59,107 @@ export const createPost = async (req, res) => {
       description,
       inExchangeFor,
       images: uploadedUrls,
-      author: _id
-    }
+      author: _id,
+    };
 
-    if(hasLocation){
+    if (hasLocation) {
       postData.location = {
         type: "Point",
-        coordinates: [lon, lat]
-      }
+        coordinates: [lon, lat],
+      };
     }
 
     await Post.create(postData);
 
     return res.status(201).json({
       success: true,
-      message: "Post created successfully."
-    })
-  } catch(e){
+      message: "Post created successfully.",
+    });
+  } catch (e) {
     console.error("Error in createPost: ", e);
     res.status(500).json({
       success: false,
       message: "Internal Server Error",
-    })
+    });
   }
-}
+};
 
 export const updatePost = async (req, res) => {
-  try{
+  try {
     const { id } = req.params;
-    const { title, type, description, inExchangeFor, longitude, latitude} = req.body;
+    const { title, type, description, inExchangeFor, longitude, latitude } =
+      req.body;
     const keptImageIds = JSON.parse(req.body.keptImageIds || "[]");
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).json({
         success: false,
-        message: "Invalid post ID."
-      })
+        message: "Invalid post ID.",
+      });
     }
 
     const post = await Post.findOne({
       _id: id,
-      author: req.user._id
+      author: req.user._id,
     }).select("+images.publicId");
 
-    if(!post){
+    if (!post) {
       return res.status(404).json({
         success: false,
-        message: "Post not found."
+        message: "Post not found.",
       });
     }
 
-    if((keptImageIds?.length || 0) + (req.files?.length || 0) > 5){
+    if ((keptImageIds?.length || 0) + (req.files?.length || 0) > 5) {
       return res.status(400).json({
         success: false,
-        message: "Images limit per post is 5"
+        message: "Images limit per post is 5",
       });
     }
 
-    if(title){
+    if (title) {
       post.title = title;
     }
 
-    if(type){
+    if (type) {
       post.type = type;
     }
 
-    if(description){
+    if (description) {
       post.description = description;
     }
 
-    if(inExchangeFor){
+    if (inExchangeFor) {
       post.inExchangeFor = inExchangeFor;
     }
 
-    if(longitude != undefined && latitude != undefined){
-      if(validateCoordinates(Number(latitude), Number(longitude))){
+    if (longitude != undefined && latitude != undefined) {
+      if (validateCoordinates(Number(latitude), Number(longitude))) {
         post.location = {
           type: "Point",
-          coordinates: [Number(longitude), Number(latitude)]
-        }
+          coordinates: [Number(longitude), Number(latitude)],
+        };
       } else {
         return res.status(400).json({
           success: false,
-          message: "Invalid location coordinates."
+          message: "Invalid location coordinates.",
         });
       }
     }
 
-    const imagesToDelete = post.images.filter(img => !keptImageIds.includes(img._id.toString()));
+    const imagesToDelete = post.images.filter(
+      (img) => !keptImageIds.includes(img._id.toString()),
+    );
     await deleteImages(imagesToDelete);
 
-    const existingImages = post.images.filter(img => keptImageIds.includes(img._id.toString()));
+    const existingImages = post.images.filter((img) =>
+      keptImageIds.includes(img._id.toString()),
+    );
 
     const newFiles = req.files || [];
 
-    const uploadedImages = newFiles.length > 0 
-    ? await uploadImages(newFiles)
-    : [];
+    const uploadedImages =
+      newFiles.length > 0 ? await uploadImages(newFiles) : [];
 
     post.images = [...existingImages, ...uploadedImages];
 
@@ -164,41 +167,41 @@ export const updatePost = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      message: "Post updated successfully."
-    })
-  } catch(err){
+      message: "Post updated successfully.",
+    });
+  } catch (err) {
     console.error("Error in updatePost: ", err);
     return res.status(500).json({
       success: false,
-      message: "Internal server error"
-    })
+      message: "Internal server error",
+    });
   }
-}
+};
 
 export const deletePost = async (req, res) => {
-  try{
-    const {id} = req.params;
+  try {
+    const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).json({
         success: false,
-        message: "Invalid post ID."
-      })
+        message: "Invalid post ID.",
+      });
     }
 
     const post = await Post.findOne({
       _id: id,
-      author: req?.user?._id
+      author: req?.user?._id,
     }).select("+images.publicId");
 
-    if(!post){
+    if (!post) {
       return res.status(404).json({
         success: false,
-        message: "Post not found."
-      })
+        message: "Post not found.",
+      });
     }
 
-    if(post.images?.length > 0){
+    if (post.images?.length > 0) {
       await deleteImages(post.images);
     }
 
@@ -206,66 +209,98 @@ export const deletePost = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      message: "Post deleted successfully."
-    })
-  } catch(err){
+      message: "Post deleted successfully.",
+    });
+  } catch (err) {
     console.error("Error in deletePost: ", err);
     res.status(500).json({
       success: false,
-      message: "Internal Server Error."
-    })
+      message: "Internal Server Error.",
+    });
   }
-}
+};
 
 export const getPosts = async (req, res) => {
-  try{
-    
+  try {
+    const { search = "", type, sort, order = "desc" } = req.query;
 
-  } catch(err){
+    const allowedSortFields = ["createdAt", "title"];
+    if (!allowedSortFields.includes(sort)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid sort field.",
+      });
+    }
+
+    const allowedTypes = ["online", "offline"];
+    if (type && !allowedTypes.includes(type)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid type filter.",
+      });
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const sortOrder = order === "asc" ? 1 : -1;
+    const offset = (page - 1) * limit;
+
+    const posts = await Post.find({
+      title: { $regex: search, $options: "i" },
+      type: type,
+    })
+      .sort({ [sort]: sortOrder })
+      .skip(offset)
+      .limit(limit);
+
+    res.status(200).json({
+      success: true,
+      message: "Posts sent successfully",
+      data: posts,
+    });
+  } catch (err) {
     console.error("Error in getPosts: ", err);
     return res.status(500).json({
       success: false,
-      message: "Internal server error."
-    })
+      message: "Internal server error.",
+    });
   }
-
-
-}
+};
 
 export const getPostById = async (req, res) => {
-  try{
-    const {id} = req.params;
+  try {
+    const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).json({
         success: false,
-        message: "Invalid post ID."
+        message: "Invalid post ID.",
       });
     }
 
     const post = await Post.findOne({
       _id: id,
-      author: req.user?._id
+      author: req.user?._id,
     });
 
-    if(!post){
+    if (!post) {
       return res.status(404).json({
         success: false,
-        message: "Post not found."
+        message: "Post not found.",
       });
     }
 
     res.status(200).json({
       success: true,
       message: "Post found.",
-      data: post
-    })
-  } catch(err){
+      data: post,
+    });
+  } catch (err) {
     console.error("Error in getPostById: ", err);
     res.status(500).json({
       success: false,
-      message: "Internal server error."
+      message: "Internal server error.",
     });
   }
-}
-
+};
